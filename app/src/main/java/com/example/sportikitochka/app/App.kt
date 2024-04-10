d385b9eff663fe26174f100c99586725a4c554ae
Wasilewski6860 <trollivanovich6860@gmail.com>
2024-04-10 19:50:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sportikitochka/app/App.kt
@@ -0,0 +1,23 @@
+package com.example.sportikitochka.app
+
+import android.app.Application
+import android.widget.Toast
+import com.example.sportikitochka.di.appModule
+import com.yandex.mapkit.MapKitFactory
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@App)
+            androidLogger(Level.DEBUG)
+            modules(listOf(appModule))
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/sportikitochka/presentation/splash/SplashViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.sportikitochka.presentation.splash
+
+import androidx.lifecycle.ViewModel
+
+class SplashViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
